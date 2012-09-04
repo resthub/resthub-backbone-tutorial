9eb3f0a0cb198193fed0460ec8e65832bfeeab1d
Loïc Frering <loic.frering@gmail.com>
2012-09-04 18:30:38 +0200

    Bind collection's add event on TasksView's render.
    Add a nice fade effect.

--- js/views/tasks.js
+++ js/views/tasks.js
@@ -1,8 +1,14 @@
 define(['backbone', 'hbs!templates/tasks'], function(Backbone, tasksTemplate) {
 
   var TasksView = Backbone.View.extend({
+    initialize: function() {
+      this.collection.on('add', this.render, this);
+    },
     render: function() {
-      this.$el.html(tasksTemplate(this.collection.toJSON()));
+      this.$el.fadeOut(function() {
+        this.$el.html(tasksTemplate(this.collection.toJSON()));
+        this.$el.fadeIn();
+      }.bind(this));
       return this;
     }
   });
