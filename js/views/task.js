cf035923166da98fd235096df441d6c58cc070f9
Loïc Frering <loic.frering@gmail.com>
2012-09-04 16:13:57 +0200

    Bind model's change event on render.

--- js/views/task.js
+++ js/views/task.js
@@ -1,6 +1,9 @@
 define(['backbone', 'hbs!templates/task'], function(Backbone, taskTemplate) {
 
   var TaskView = Backbone.View.extend({
+    initialize: function() {
+      this.model.on('change', this.render, this);
+    },
     render: function() {
       this.$el.html(taskTemplate(this.model.toJSON()));
       return this;
